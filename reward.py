316a579cc3f9c7676e21d6ced1e24837167f0da3
Bla-De <52192194+Bla-De@users.noreply.github.com>
2021-04-16 11:12:55 +1200

    Delete reward.py

--- reward.py
+++
@@ -1,7 +0,0 @@
-class Reward():
-    def __init__(self):
-        self.externalId = None
-        self.redeemed = False
-        self.valid = None
-        self.user = ""
-        self.character = None
